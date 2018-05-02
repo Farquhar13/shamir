b7420c3b5bb34cf5de46dab346c705c94d51faeb
doubravam524 <doubravam524@strose.edu>
2018-05-02 15:25:17 -0400

    Merge branch 'master' of /home/doubravam524/IdeaProjects/shamir with conflicts.

--- src/main/java/com/codahale/shamir/driver.java
+++ src/main/java/com/codahale/shamir/driver.java
@@ -11,15 +11,14 @@ public class driver {
         // specify shares (n) and threshold (k)
         final Scheme scheme = Scheme.of(5, 3);
         // hardcoded secret string calling getBytes
-        final byte[] secret = "123".getBytes(StandardCharsets.UTF_8);
+        final byte[] secret = "I am a secret123".getBytes(StandardCharsets.UTF_8);
 
         /*
          * creates a Map object called parts that contains the shares
          * requires calling scheme.split with the secret as parameter
          */
         final Map<Integer, byte[]> parts = scheme.split(secret);
-
-        final Map<Integer, byte[]> newParts = new HashMap<>(5);
+        System.out.println("parts: " + parts);
 
         /*
          * recovered is the reconstructed secret
@@ -30,17 +29,5 @@ public class driver {
         final byte[] recovered = scheme.join(parts);
         System.out.println("recovered secret: " + new String(recovered, StandardCharsets.UTF_8));
     }
-
-    /*
-    final Map<Integer, byte[]> shares = new HashMap<>(5);
-    public void reconstruct(int key, byte[] share){
-        shares.replace(key, share);
-        k_check = 0;
-        for (i=0; i <=shares.length(
-            //check how may shares are in the map object, if greater than k, send to scheme.join
-        }
-    }
-*/
-
 
 }
